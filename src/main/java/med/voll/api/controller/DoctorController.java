f2e33c23da234050e498accd17ed033396839044
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-05 04:22:30 -0600

    feat: update doctor controller and response handling

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -2,12 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.doctor.*;
+import med.voll.api.domain.address.AddressData;
+import med.voll.api.domain.doctor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/doctors")
@@ -17,35 +22,45 @@ public class DoctorController {
     private DoctorRepository doctorRepository;
 
     @PostMapping
-    public void registerDoctor(@RequestBody @Valid DoctorData doctorData) {
-        doctorRepository.save(new Doctor(doctorData));
+    public ResponseEntity<DoctorResponseData> registerDoctor(@RequestBody @Valid DoctorData doctorData, UriComponentsBuilder uriComponentsBuilder) {
+        Doctor doctor = doctorRepository.save(new Doctor(doctorData));
+        DoctorResponseData doctorResponseData = createDoctorResponseData(doctor);
+        URI url = uriComponentsBuilder.path("/doctors/{id}").buildAndExpand(doctor.getId()).toUri();
+        return ResponseEntity.created(url).body(doctorResponseData);
     }
 
     @GetMapping
-    public Page<ListDoctorData> listDoctors(@PageableDefault(size = 5) Pageable pageable) {
-        return doctorRepository.findByActiveTrue(pageable)
-                .map(ListDoctorData::new);
+    public ResponseEntity<Page<ListDoctorData>> listDoctors(@PageableDefault(size = 5) Pageable pageable) {
+        return ResponseEntity.ok(doctorRepository.findByActiveTrue(pageable).map(ListDoctorData::new));
     }
 
     @PutMapping
     @Transactional
-    public void updateDoctor(@RequestBody @Valid UpdateDoctorData updateDoctorData) {
+    public ResponseEntity<DoctorResponseData> updateDoctor(@RequestBody @Valid UpdateDoctorData updateDoctorData) {
         Doctor doctor = doctorRepository.getReferenceById(updateDoctorData.id());
         doctor.updateDoctor(updateDoctorData);
+        return ResponseEntity.ok(createDoctorResponseData(doctor));
     }
 
-    // LOGICAL DELETE
     @DeleteMapping("/{id}")
     @Transactional
-    public void deleteDoctor(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
         Doctor doctor = doctorRepository.getReferenceById(id);
         doctor.desactiveDoctor();
+        return ResponseEntity.noContent().build();
     }
 
-    /*
-    DATABASE DELETE
-    public void deleteDoctor(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    @Transactional
+    public ResponseEntity<DoctorResponseData> getDoctor(@PathVariable Long id) {
         Doctor doctor = doctorRepository.getReferenceById(id);
-        doctorRepository.delete(doctor);
-    }*/
+        return ResponseEntity.ok(createDoctorResponseData(doctor));
+    }
+
+    private DoctorResponseData createDoctorResponseData(Doctor doctor) {
+        return new DoctorResponseData(doctor.getId(), doctor.getName(), doctor.getEmail(), doctor.getPhone(), doctor.getDocument(),
+                new AddressData(doctor.getAddress().getStreet(), doctor.getAddress().getNumber(), doctor.getAddress().getComplement(), doctor.getAddress().getDistrict(), doctor.getAddress().getCity())
+        );
+    }
+
 }
