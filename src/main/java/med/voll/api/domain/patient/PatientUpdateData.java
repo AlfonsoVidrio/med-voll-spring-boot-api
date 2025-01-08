78eee7d6051a35e347e98191eecd86d49e5aa012
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:28 -0600

    feat: add PatientUpdateData for updating patient data

--- src/main/java/med/voll/api/domain/patient/PatientUpdateData.java
+++ src/main/java/med/voll/api/domain/patient/PatientUpdateData.java
@@ -1,4 +1,14 @@
 package med.voll.api.domain.patient;
 
-public record PatientUpdateData() {
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.address.AddressData;
+import med.voll.api.domain.address.AddressUpdateData;
+
+public record PatientUpdateData(
+        @NotNull
+        Long id,
+        String name,
+        String phone,
+        AddressUpdateData address
+) {
 }
