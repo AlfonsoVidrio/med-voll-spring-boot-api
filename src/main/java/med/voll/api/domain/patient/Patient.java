64ec41d065b2a45a7c8bf5f924512422241909da
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:17 -0600

    feat: add Patient entity for patient management

--- src/main/java/med/voll/api/domain/patient/Patient.java
+++ src/main/java/med/voll/api/domain/patient/Patient.java
@@ -1,4 +1,52 @@
 package med.voll.api.domain.patient;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.address.Address;
+import med.voll.api.domain.address.AddressUpdateData;
+
+@Table(name = "patients")
+@Entity(name = "Patient")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Patient {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String document;
+    @Embedded
+    private Address address;
+    private boolean active;
+
+    public Patient(PatientData patientData) {
+        this.active = true;
+        this.name = patientData.name();
+        this.email = patientData.email();
+        this.phone = patientData.phone();
+        this.document = patientData.document();
+        this.address = new Address(patientData.address());
+    }
+
+    public void updatePatient(PatientUpdateData patientData) {
+        if (patientData.name() != null) {
+            this.name = patientData.name();
+        }
+        if (patientData.phone() != null) {
+            this.phone = patientData.phone();
+        }
+        if (patientData.address() != null) {
+            this.address.updateAddress(patientData.address());
+        }
+    }
+
+    public void delete() {
+        this.active = false;
+    }
 }
