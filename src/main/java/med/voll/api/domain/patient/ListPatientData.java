7ef070b51d531c4da483f8d527317322cf59611b
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:13 -0600

    feat: add ListPatientData for listing patient details

--- src/main/java/med/voll/api/domain/patient/ListPatientData.java
+++ src/main/java/med/voll/api/domain/patient/ListPatientData.java
@@ -1,4 +1,12 @@
 package med.voll.api.domain.patient;
 
-public record ListPatientData() {
+public record ListPatientData(
+        Long id,
+        String name,
+        String email,
+        String document
+) {
+    public ListPatientData(Patient patient) {
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getDocument());
+    }
 }
