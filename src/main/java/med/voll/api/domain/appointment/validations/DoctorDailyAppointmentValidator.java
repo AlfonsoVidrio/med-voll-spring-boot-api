e48497ea5ecacf5412e9e8d1811ef27a3d74097a
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:50:58 -0600

    feat: add DoctorDailyAppointmentValidator for doctor daily appointments

--- src/main/java/med/voll/api/domain/appointment/validations/DoctorDailyAppointmentValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/DoctorDailyAppointmentValidator.java
@@ -1,4 +1,19 @@
 package med.voll.api.domain.appointment.validations;
 
-public class DoctorDailyAppointmentValidator {
+import med.voll.api.domain.appointment.AppointmentData;
+import med.voll.api.domain.appointment.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DoctorDailyAppointmentValidator implements QueryValidator{
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    public void validate(AppointmentData appointmentData) {
+        var doctorHasAppointment = appointmentRepository.existsByDoctorIdAndDate(appointmentData.doctorId(), appointmentData.date());
+        if (doctorHasAppointment) {
+            throw new IllegalArgumentException("Doctor already has an appointment scheduled for this day");
+        }
+    }
 }
