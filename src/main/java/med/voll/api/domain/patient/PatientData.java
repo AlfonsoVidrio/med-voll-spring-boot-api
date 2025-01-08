e9a7884139e770f321fda7f4e18d7f102d02a153
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:21 -0600

    feat: add PatientData for storing patient details

--- src/main/java/med/voll/api/domain/patient/PatientData.java
+++ src/main/java/med/voll/api/domain/patient/PatientData.java
@@ -1,4 +1,23 @@
 package med.voll.api.domain.patient;
 
-public record PatientData() {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.address.AddressData;
+import med.voll.api.domain.address.AddressUpdateData;
+
+public record PatientData(
+        @NotBlank
+        String name,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String phone,
+        @NotBlank
+        String document,
+        @NotNull @Valid AddressData address
+        ) {
+
 }
