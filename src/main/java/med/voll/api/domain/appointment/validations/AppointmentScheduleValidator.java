e5054937819fca2f2d811cde149fb1a87fbae001
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:50:54 -0600

    feat: add AppointmentScheduleValidator for schedule validation

--- src/main/java/med/voll/api/domain/appointment/validations/AppointmentScheduleValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/AppointmentScheduleValidator.java
@@ -1,4 +1,20 @@
 package med.voll.api.domain.appointment.validations;
 
-public class AppointmentScheduleValidator {
+import med.voll.api.domain.appointment.AppointmentData;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+
+@Component
+public class AppointmentScheduleValidator implements QueryValidator{
+
+    public void validate(AppointmentData appointmentData) {
+        var date = appointmentData.date();
+        var sunday = date.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var scheduleBeforeOpening = date.getHour() < 7;
+        var scheduleAfterClosing = date.getHour() > 18;
+        if (sunday || scheduleBeforeOpening || scheduleAfterClosing) {
+            throw new IllegalArgumentException("Invalid appointment schedule, please select a date between Monday and Saturday, from 7:00 to 18:00");
+        }
+    }
 }
