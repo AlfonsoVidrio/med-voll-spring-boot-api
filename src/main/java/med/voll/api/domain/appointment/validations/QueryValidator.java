c84e7aa07d92260a1b40b237c90d495307cf8c61
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:06 -0600

    feat: add QueryValidator for query validation

--- src/main/java/med/voll/api/domain/appointment/validations/QueryValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/QueryValidator.java
@@ -1,4 +1,7 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.appointment.AppointmentData;
+
 public interface QueryValidator {
+     void validate(AppointmentData appointmentData);
 }
