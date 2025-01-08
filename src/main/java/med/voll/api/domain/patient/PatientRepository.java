78579286d51727aadf6c626be62a1ed82f0f59e8
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:24 -0600

    feat: add PatientRepository for CRUD operations

--- src/main/java/med/voll/api/domain/patient/PatientRepository.java
+++ src/main/java/med/voll/api/domain/patient/PatientRepository.java
@@ -1,4 +1,17 @@
 package med.voll.api.domain.patient;
 
-public class PatientRepository {
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findAllByActiveTrue(Pageable pageable);
+
+    @Query("""
+        SELECT p.active FROM Patient p
+        WHERE p.id = :id
+        """)
+    boolean findActiveById(@NotNull Long id);
 }
