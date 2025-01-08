ed4c93e784e3339471824afec6d7722f90ff8605
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:50:22 -0600

    feat: add AddressUpdateData for address updates

--- src/main/java/med/voll/api/domain/address/AddressUpdateData.java
+++ src/main/java/med/voll/api/domain/address/AddressUpdateData.java
@@ -1,4 +1,12 @@
 package med.voll.api.domain.address;
 
-public class AddressUpdateData {
+import jakarta.validation.constraints.NotNull;
+
+public record AddressUpdateData(
+        String street,
+        String district,
+        String city,
+        String number,
+        String complement
+) {
 }
