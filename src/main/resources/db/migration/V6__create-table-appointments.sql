1adc77b4e4b5c7fe1635f2ffa5f5800d2ba7f719
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:46 -0600

    feat: add V6 migration to create appointments table

--- src/main/resources/db/migration/V6__create-table-appointments.sql
+++ src/main/resources/db/migration/V6__create-table-appointments.sql
@@ -1,10 +1,10 @@
 CREATE TABLE appointments (
-                              id bigint NOT NULL AUTO_INCREMENT,
-                              doctor_id bigint NOT NULL,
-                              patient_id bigint NOT NULL,
-                              date DATE NOT NULL,
+    id bigint NOT NULL AUTO_INCREMENT,
+    doctor_id bigint NOT NULL,
+    patient_id bigint NOT NULL,
+    date datetime NOT NULL,
 
-                              PRIMARY KEY (id),
-                              CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctors(id),
-                              CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id)
+    PRIMARY KEY (id),
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctors(id),
+    CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
