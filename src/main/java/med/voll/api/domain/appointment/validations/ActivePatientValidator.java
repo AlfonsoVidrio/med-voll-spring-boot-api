f495015b5a593d441cf038c78a8225a73f3b1536
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:50:50 -0600

    feat: add ActivePatientValidator for validating active patients

--- src/main/java/med/voll/api/domain/appointment/validations/ActivePatientValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/ActivePatientValidator.java
@@ -1,4 +1,21 @@
 package med.voll.api.domain.appointment.validations;
 
-public class ActivePatientValidator {
+import med.voll.api.domain.appointment.AppointmentData;
+import med.voll.api.domain.patient.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActivePatientValidator implements QueryValidator{
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public void validate(AppointmentData appointmentData) {
+        var patientIsActive = patientRepository.findActiveById(appointmentData.patientId());
+        if (!patientIsActive) {
+            throw new IllegalArgumentException("Patient is not active");
+        }
+
+    }
+
 }
