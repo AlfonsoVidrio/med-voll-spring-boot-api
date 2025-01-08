ffbbb4c23201ee931e7c4fa77b5e42f64ef6db7a
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:50:39 -0600

    feat: add AppointmentDetailsData for detailed appointment view

--- src/main/java/med/voll/api/domain/appointment/AppointmentDetailsData.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentDetailsData.java
@@ -1,4 +1,14 @@
 package med.voll.api.domain.appointment;
 
-public record AppointmentDetailsData() {
+import java.time.LocalDateTime;
+
+public record AppointmentDetailsData(
+        Long id,
+        Long doctorId,
+        Long patientId,
+        LocalDateTime date
+) {
+    public AppointmentDetailsData(Appointment query) {
+        this(query.getId(), query.getDoctor().getId(), query.getPatient().getId(), query.getDate());
+    }
 }
