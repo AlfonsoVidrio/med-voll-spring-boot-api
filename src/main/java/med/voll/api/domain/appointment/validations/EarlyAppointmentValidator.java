5ddcea7a93c5e7cec72b5f91367acd2c18e4d4c5
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:02 -0600

    feat: add EarlyAppointmentValidator for early appointment validation

--- src/main/java/med/voll/api/domain/appointment/validations/EarlyAppointmentValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/EarlyAppointmentValidator.java
@@ -1,4 +1,20 @@
 package med.voll.api.domain.appointment.validations;
 
-public class EarlyAppointmentValidator {
+import med.voll.api.domain.appointment.AppointmentData;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class EarlyAppointmentValidator implements QueryValidator {
+
+    public void validate(AppointmentData appointmentData) {
+        var dateAppointment = appointmentData.date();
+        var now = LocalDateTime.now();
+        var differenceInMinutes = Duration.between(now, dateAppointment).toMinutes();
+        if (differenceInMinutes < 30) {
+            throw new IllegalArgumentException("Invalid appointment schedule, please select a date at least 30 minutes from now");
+        }
+    }
 }
