03d5ad9cf5ba4cd0437f7246f31da0b9ad4cc471
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:51:10 -0600

    feat: add SingleAppointmentPerDayValidator for daily appointment limits

--- src/main/java/med/voll/api/domain/appointment/validations/SingleAppointmentPerDayValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/SingleAppointmentPerDayValidator.java
@@ -1,4 +1,22 @@
 package med.voll.api.domain.appointment.validations;
 
-public class SingleAppointmentPerDayValidator {
+import med.voll.api.domain.appointment.AppointmentData;
+import med.voll.api.domain.appointment.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingleAppointmentPerDayValidator implements QueryValidator {
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    public void validate(AppointmentData appointmentData) {
+        var firstAppointment = appointmentData.date().withHour(7);
+        var lastAppointment = appointmentData.date().withHour(18);
+        boolean patientHasAnotherAppointment = appointmentRepository.existsByPatientIdAndDateBetween(appointmentData.patientId(), firstAppointment, lastAppointment);
+        System.out.println("patientHasAnotherAppointment = " + patientHasAnotherAppointment);
+        if (patientHasAnotherAppointment) {
+            throw new IllegalArgumentException("Patient already has an appointment scheduled for this day");
+        }
+    }
 }
