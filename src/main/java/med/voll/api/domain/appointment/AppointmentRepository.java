552dec4282d477e3a59680f33460b062e8f048e0
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:45:31 -0600

    feat: add AppointmentRepository for handling appointment data

--- src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
@@ -1,4 +1,13 @@
 package med.voll.api.domain.appointment;
 
-public interface AppointmentRepository {
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    boolean existsByPatientIdAndDateBetween(Long patientId, LocalDateTime startDate, LocalDateTime endDate);
+
+    boolean existsByDoctorIdAndDate(Long doctorId, LocalDateTime date);
 }
