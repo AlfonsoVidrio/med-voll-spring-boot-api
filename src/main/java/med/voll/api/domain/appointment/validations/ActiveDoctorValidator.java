8972766cd944d715960767b316f4c55c42b7b55b
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-07 22:50:47 -0600

    feat: add ActiveDoctorValidator for validating active doctors

--- src/main/java/med/voll/api/domain/appointment/validations/ActiveDoctorValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/ActiveDoctorValidator.java
@@ -1,4 +1,22 @@
 package med.voll.api.domain.appointment.validations;
 
-public class ActiveDoctorValidator {
+import med.voll.api.domain.appointment.AppointmentData;
+import med.voll.api.domain.doctor.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveDoctorValidator implements QueryValidator {
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public void validate(AppointmentData appointmentData) {
+        if (appointmentData.doctorId() == null) {
+            return;
+        }
+        var doctorIsActive = doctorRepository.findActiveById(appointmentData.doctorId());
+        if (!doctorIsActive) {
+            throw new IllegalArgumentException("Doctor is not active");
+        }
+    }
 }
