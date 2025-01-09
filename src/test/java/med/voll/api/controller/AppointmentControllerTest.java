011dd74e07b79d1212f3861859280d01fff5f8d5
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-08 19:02:16 -0600

    test: add tests for appointment and doctor repository

--- src/test/java/med/voll/api/controller/AppointmentControllerTest.java
+++ src/test/java/med/voll/api/controller/AppointmentControllerTest.java
@@ -1,4 +1,74 @@
-import static org.junit.jupiter.api.Assertions.*;
+package med.voll.api.controller;
+
+import med.voll.api.domain.appointment.AppointmentBooking;
+import med.voll.api.domain.appointment.AppointmentData;
+import med.voll.api.domain.appointment.AppointmentDetailsData;
+import med.voll.api.domain.doctor.Specialty;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class AppointmentControllerTest {
-  
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private JacksonTester<AppointmentData> appointmentDataJsonTester;
+    @Autowired
+    private JacksonTester<AppointmentDetailsData> appointmentDetailsDataJsonTester;
+    @MockBean
+    private AppointmentBooking reservation;
+
+    @Test
+    @DisplayName("should return status 400 when the appointment data is empty")
+    @WithMockUser// This annotation is used to simulate a user logged in
+    void reserve_scenario1() throws Exception {
+        var response = mvc.perform(post("/appointments"))
+                .andReturn().getResponse();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatus());
+    }
+
+    @Test
+    @DisplayName("should return status 200 when the appointment data is valid")
+    @WithMockUser// This annotation is used to simulate a user logged in
+    void reserve_scenario2() throws Exception {
+        var date = LocalDateTime.now().plusHours(1);
+        var specialty = Specialty.CARDIOLOGIA;
+        var detailsData = new AppointmentDetailsData(null, 2l, 5l, date);
+        when(reservation.reserve(any())).thenReturn(detailsData);
+
+        var response = mvc.perform(post("/appointments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(appointmentDataJsonTester.write(
+                                new AppointmentData(2l, 5l, date, specialty)
+                        ).getJson()))
+
+                .andReturn().getResponse();
+
+        var expectedJson = appointmentDetailsDataJsonTester.write(
+                new AppointmentDetailsData(null, 2l, 5l, date)
+        ).getJson();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(expectedJson, response.getContentAsString());
+    }
 }
--- src/test/java/med/voll/api/domain/doctor/DoctorRepositoryTest.java
+++ src/test/java/med/voll/api/domain/doctor/DoctorRepositoryTest.java
@@ -1,4 +1,122 @@
+package med.voll.api.domain.doctor;
+
+
+import jakarta.persistence.EntityManager;
+import med.voll.api.domain.address.AddressData;
+import med.voll.api.domain.appointment.Appointment;
+import med.voll.api.domain.patient.Patient;
+import med.voll.api.domain.patient.PatientData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class DoctorRepositoryTest {
-  
+
+    @Autowired
+    private DoctorRepository repository;
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    @DisplayName("should return null when the doctor searched exists but is not available on the date")
+    void selectRandomAvailableDoctorOnDateScenario1() {
+        // given o arrange
+        var nextMondayAt10 = LocalDateTime.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).toLocalDate().atTime(10, 0);
+
+        var doctor = registerDoctor("doctor1", "doctor1@gmail.com", "123456", "3781463146");
+        var patient = registerPatient("pacient1", "pacient1@gmail.com", "271629", "3781122892");
+        registerAppointment(doctor, patient, nextMondayAt10);
+        // when o act
+        var doctorAvailable = repository.selectRandomAvailableDoctorOnDate(Specialty.CARDIOLOGIA, nextMondayAt10);
+        // then o assert
+        assertNull(doctorAvailable);
+    }
+
+    @Test
+    @DisplayName("should return doctor when the doctor searched exists and is available on the date")
+    void selectRandomAvailableDoctorOnDateScenario2() {
+        // given o arrange
+        var nextMondayAt10 = LocalDateTime.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).toLocalDate().atTime(10, 0);
+        var doctor = registerDoctor("doctor1", "doctor1@gmail.com", "123456", "3781463146");
+        // when o act
+        var doctorAvailable = repository.selectRandomAvailableDoctorOnDate(Specialty.CARDIOLOGIA, nextMondayAt10);
+        // then o assert
+        assertEquals(doctorAvailable, doctor);
+    }
+
+    private void registerAppointment(Doctor doctor, Patient patient, LocalDateTime date) {
+        em.persist(new Appointment(null, doctor, patient, date));
+    }
+
+    private Doctor registerDoctor(String name, String email, String document, String phone) {
+        var doctor = new Doctor(new DoctorData(
+                name,
+                email,
+                document,
+                phone,
+                Specialty.CARDIOLOGIA,
+                addressData()
+        ));
+        em.persist(doctor);
+        return doctor;
+    }
+
+    private Patient registerPatient(String name, String email, String document, String phone) {
+        var patient = new Patient(new PatientData(
+                name,
+                email,
+                document,
+                phone,
+                addressData()
+        ));
+        em.persist(patient);
+        return patient;
+    }
+
+
+
+    private DoctorData registerDoctorData() {
+        return new DoctorData(
+                "name",
+                "email",
+                "password",
+                "123456789",
+                Specialty.CARDIOLOGIA,
+                addressData()
+        );
+    }
+
+    private PatientData registerPatientData() {
+        return new PatientData(
+                "name",
+                "email",
+                "password",
+                "123456789",
+                addressData()
+        );
+    }
+
+    private AddressData addressData() {
+        return new AddressData(
+                "calle x",
+                "distrito x",
+                "ciudad x",
+                "123",
+                "1"
+        );
+    }
 }
